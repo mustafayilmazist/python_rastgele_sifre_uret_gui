5de0b6d437e46d257768f10f92bed1f4514f38e9
mustafa yılmaz <mustafayilmazbilgisayar@gmail.com>
2017-11-15 12:00:30 +0300

    eklendi:kodların tamamı

--- randompassword.py
+++ randompassword.py
@@ -1,1 +1,24 @@
 # -*- conding: utf-8 -*-
+from Tkinter import *
+import random
+import string
+myPencere = Tk()
+myPencere.geometry("400x300+100+200")
+L1 = Label(myPencere, text="Sifre burada gozukecek")
+L1.pack( side = LEFT)
+E1 = Entry(myPencere, bd =1)
+E1.pack(side = RIGHT)
+def uret():
+    basamak = E1.get()
+    myMetin = ""
+    for x in range(0, int(basamak) ):
+        myMetin += random.choice(string.ascii_letters+string.digits)
+    print "uretilen sifre:::", myMetin
+    L1.config(text=myMetin)
+def kaydet():
+    E1.config(text="abah")
+myLable = Label(myPencere)
+myLable.pack()
+btnEkle = Button(text="Uret", command=uret)
+btnEkle.pack()
+mainloop()
